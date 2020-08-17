7762c19a63b57f6a8fbd2674b1b0880b3659ae14
wowsomeco <wowsome.co@gmail.com>
2020-08-17 09:27:10 +0800

    macro for non editor only for android helper, otherwise it triggers errors on the editor

--- scripts/WowStore_Android.cs
+++ scripts/WowStore_Android.cs
@@ -5,15 +5,21 @@ namespace Wowsome {
   namespace Store {
     public class WowStore_Android : IStoreController {
       public class AndroidHelper {
+#if !UNITY_EDITOR
         AndroidJavaClass m_pluginClass = null;
+#endif
 
         public AndroidHelper() {
+#if !UNITY_EDITOR
           AndroidJavaClass playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
           AndroidJavaClass m_pluginClass = new AndroidJavaClass("wowsome.co.purchasing");
+#endif
         }
 
         public void CallMethod(string methodName, params object[] args) {
+#if !UNITY_EDITOR
           m_pluginClass.CallStatic(methodName, args);
+#endif
         }
       }
 
